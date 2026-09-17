ed342ba7a914a0b5cb206292ebe38e0027d764c3
63472974-arte <63472974@institutourusayhua.edu.pe>
2026-09-17 10:59:16 -0500

    Mejora mensaje inicial

--- semana6.py
+++ semana6.py
@@ -1,2 +1,4 @@
 #BIENVENIDO A MI REPOSITORIO DE GITHUB
 print("Hola, bienvenido a mi repositorio de GitHub. Aquí encontrarás mis proyectos y contribuciones. ¡Explora y disfruta!")
+
+print("HOLA DESDE LA RAMA mensaje-mejora")
