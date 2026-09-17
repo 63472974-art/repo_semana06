0d9db14f3c55e5b4782ff9d2f9da65a46521bd02
siete <63472974@institutourusayhua.edu.pe>
2026-09-17 15:09:38 -0500

    agregado carrera y semestre

--- semana6.py
+++ semana6.py
@@ -2,7 +2,13 @@ nombre = input("Ingrese el nombre de la persona: ")
 edad = int(input("Ingrese la edad de la persona: "))
 altura = float(input("Ingrese la altura de la persona (en metros): "))
 
+#nuevo campo agreagdo
+carrera = input("Ingrese la carrera de la persona: ")
+semestre = input("Ingrese el semestre de la persona: ")
+
 print("\n---DATOS REGISTRADOS---")
 print("Nombre:", nombre)
 print("Edad:", edad)
-print("Altura:",altura)
+print("Altura:", altura)
+print("Carrera:", carrera)
+print("semestre:", semestre)
